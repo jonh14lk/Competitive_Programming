4ef3f8e35491a66b769982d568d8c9f88f50538a
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-04-02 16:02:41 -0300

    Add files via upload

---
+++ Problemas comuns/Contagem_de_inversoes.cpp
@@ -0,0 +1,83 @@
+// seja S = a1, a2 , ... , an
+// uma inversão S é um par (i,j) com i < j e ai > aj
+
+// Solução O(n²) nao ideal:
+//for(int i=0;i<n;i++)
+//	for(int j=i+1;j<n;j++)
+//		if(v[i]>v[j]) ans++;
+
+// Em vez de trabalharmos com o vetor inteiro(n²), vamos dividir o vetor ao meio e trabalhar com suas metades,
+// que chamaremos de u1 e u2.
+
+// Queremos saber o valor de inv, o número de inversões em v. Há três tipos de inversões (i,j)(i,j) em v:
+// aquelas em que i e j estão ambos em u1, aquelas em que i e j estão ambos em u2 e aquelas
+// em que i está em u1 e j está em u2.
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lli long long int
+#define pb push_back
+#define in insert
+#define pi pair<int, int>
+#define pii pair<int, pi>
+#define mp make_pair
+#define fir first
+#define sec second
+#define MAXN 100001
+#define INF 1000000000
+
+int merge_sort(vector<int> &v)
+{
+  int ans = 0;
+
+  if (v.size() == 1)
+  {
+    return 0;
+  }
+
+  vector<int> u1, u2;
+
+  for (int i = 0; i < v.size() / 2; i++)
+  {
+    u1.pb(v[i]);
+  }
+  for (int i = v.size() / 2; i < v.size(); i++)
+  {
+    u2.pb(v[i]);
+  }
+
+  ans += merge_sort(u1);
+  ans += merge_sort(u2);
+
+  u1.pb(INF);
+  u2.pb(INF);
+
+  int ini1 = 0, ini2 = 0;
+
+  for (int i = 0; i < v.size(); i++)
+  {
+    if (u1[ini1] <= u2[ini2])
+    {
+      v[i] = u1[ini1];
+      ini1++;
+    }
+    else
+    {
+      v[i] = u2[ini2];
+      ini2++;
+      ans += u1.size() - ini1 - 1;
+    }
+  }
+
+  return ans;
+}
+signed main()
+{
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  cout << merge_sort(v) << endl;
+  return 0;
+}
---
+++ Problemas comuns/lcs.cpp
@@ -0,0 +1,49 @@
+//Dadas duas sequências s1 e s2, uma de tamanho n e outra de tamanho m, qual a maior subsequência comum às duas?
+
+// uma subsequência de s é um subconjunto dos elementos de s na mesma ordem em que apareciam antes.
+// isto significa que {1, 3, 5} é uma subsequência de {1, 2, 3, 4, 5}, mesmo 1 não estando do lado do 3.
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lli long long int
+#define pb push_back
+#define in insert
+#define pi pair<int, int>
+#define pii pair<int, pi>
+#define mp make_pair
+#define fir first
+#define sec second
+#define MAXN 1001
+#define INF 1000000000
+
+int v1[MAXN];
+int v2[MAXN];
+int dp[MAXN][MAXN];
+
+void lcs(int m, int n)
+{
+  for (int i = 0; i <= m; i++)
+  {
+    for (int j = 0; j <= n; j++)
+    {
+      if (i == 0 || j == 0) //se uma das sequências for vazia
+        dp[i][j] = 0;
+      else if (v1[i - 1] == v2[j - 1]) // se eh igual, adiciono a lcs e subtraio dos dois
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      else
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); // se nao retorno o maximo entre tirar um dos dois caras
+    }
+  }
+  cout << dp[m][n] << endl;
+}
+signed main()
+{
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < n; i++)
+    cin >> v1[i];
+  for (int i = 0; i < m; i++)
+    cin >> v2[i];
+  lcs(n, m);
+  return 0;
+}
