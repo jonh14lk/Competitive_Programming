30d56e6b10628ecf4008572cac6391d7ca82830b
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-04-16 23:23:05 -0300

    Create Aplications.cpp

---
+++ Binary Search/Aplications.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lli long long int
+#define pb push_back
+#define in insert
+#define pi pair<int, int>
+#define pii pair<int, pi>
+#define mp make_pair
+#define fir first
+#define sec second
+#define MAXN 1001
+
+// 1 - ts para double
+long double ts()
+{
+  long double l = 0, r = DBL_MAX;
+  for (int i = 0; i < 2000; i++)
+  {
+    long double l1 = (l * 2 + r) / 3.0;
+    long double l2 = (l + 2 * r) / 3.0;
+    if (possible(l1))
+      r = l2;
+    else
+      l = l1;
+  }
+  return l;
+}
+// 2- bb para double
+long double bb()
+{
+  long double i = 0, f = DBL_MAX, m;
+  while (f - i > 0.000000001)
+  {
+    m = (i + f) / 2.0;
+    if (possible(m))
+      f = m;
+    else
+      i = m;
+  }
+  return i;
+}
+// 3 - bb pra int
+lli bb()
+{
+  lli i = 0, f = INT_MAX, m;
+  while (i < f)
+  {
+    m = (i + f) / 2;
+    if (possible(m))
+      f = m;
+    else
+      i = m + 1;
+  }
+  return i;
+}
