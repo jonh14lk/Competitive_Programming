23dd654fa3cd480f60a6da1b29e868a1635e39a6
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-07-07 15:07:10 -0300

    better implementation

--- Structures/segtree_lazy.cpp
+++ Structures/segtree_lazy.cpp
@@ -17,67 +17,53 @@ vector<int> seg(4 * MAXN);
 vector<int> lazy(4 * MAXN);
 vector<int> v(MAXN);
 
+void add(int i, int l, int r, int diff)
+{
+  seg[i] += (r - l + 1) * diff;
+  if (l != r)
+  {
+    lazy[l << 1] += diff;
+    lazy[(l << 1) | 1] += diff;
+  }
+}
 void update(int i, int l, int r, int ql, int qr, int diff)
 {
   if (lazy[i] != 0)
-  {
-    seg[i] += (r - l + 1) * lazy[i];
-    if (l != r)
-    {
-      lazy[i * 2 + 1] += lazy[i];
-      lazy[i * 2 + 2] += lazy[i];
-    }
-    lazy[i] = 0;
-  }
+    add(i, l, r, lazy[i]), lazy[i] = 0;
   if (l > r || l > qr || r < ql)
     return;
   if (l >= ql && r <= qr)
   {
-    seg[i] += (r - l + 1) * diff;
-    if (l != r)
-    {
-      lazy[i * 2 + 1] += diff;
-      lazy[i * 2 + 2] += diff;
-    }
+    add(i, l, r, diff);
     return;
   }
-  int mid = (l + r) / 2;
-  update(i * 2 + 1, l, mid, ql, qr, diff);
-  update(i * 2 + 2, mid + 1, r, ql, qr, diff);
-  seg[i] = seg[i * 2 + 1] + seg[i * 2 + 2];
+  int mid = (l + r) >> 1;
+  update(i << 1, l, mid, ql, qr, diff);
+  update((i << 1) | 1, mid + 1, r, ql, qr, diff);
+  seg[i] = (seg[i << 1] + seg[(i << 1) | 1]) % mod;
 }
 int query(int l, int r, int ql, int qr, int i)
 {
+  int mid = (l + r) >> 1;
   if (lazy[i] != 0)
-  {
-    seg[i] += (r - l + 1) * lazy[i];
-    if (l != r)
-    {
-      lazy[i * 2 + 1] += lazy[i];
-      lazy[i * 2 + 2] += lazy[i];
-    }
-    lazy[i] = 0;
-  }
+    add(i, l, r, lazy[i]), lazy[i] = 0;
   if (l > r || l > qr || r < ql)
     return 0;
   if (l >= ql && r <= qr)
     return seg[i];
-  int mid = (l + r) / 2;
-  return query(l, mid, ql, qr, 2 * i + 1) + query(mid + 1, r, ql, qr, 2 * i + 2);
+  return (query(l, mid, ql, qr, i << 1) + query(mid + 1, r, ql, qr, (i << 1) | 1)) % mod;
 }
 void build(int l, int r, int i)
 {
-  if (l > r)
-    return;
   if (l == r)
   {
     seg[i] = v[l];
     return;
   }
-  int mid = (l + r) / 2;
-  build(l, mid, 2 * i + 1);
-  build(mid + 1, r, 2 * i + 2);
-  seg[i] = seg[i * 2 + 1] + seg[i * 2 + 2];
+  int mid = (l + r) >> 1;
+  build(l, mid, i << 1);
+  build(mid + 1, r, (i << 1) | 1);
+  seg[i] = (seg[i << 1] + seg[(i << 1) | 1]) % mod;
 }
 signed main()
 {
@@ -85,7 +71,7 @@ signed main()
   cin >> n;
   for (int i = 0; i < n; i++)
     cin >> v[i];
-  build(0, n - 1, 0);
+  build(0, n - 1, 1);
   int q;
   cin >> q;
   while (q--)
@@ -94,8 +80,8 @@ signed main()
     char t;
     cin >> t >> l >> r;
     if (t == 'Q')
-      cout << query(0, n - 1, l, r, 0) << endl;
+      cout << query(0, n - 1, l, r, 1) << endl;
     else
-      update(0, 0, n - 1, l, l, r - v[l]), v[l] += (r - v[l]);
+      update(1, 0, n - 1, l, l, r - v[l]), v[l] += (r - v[l]);
   }
 }
