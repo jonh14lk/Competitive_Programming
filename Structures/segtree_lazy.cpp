0ad25ec2f0151c278ccb05734824f3006d69404a
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-08-03 12:07:00 -0300

    change my segtree with lazy propagation template

--- Structures/segtree_lazy.cpp
+++ Structures/segtree_lazy.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define pii pair<pi, int>
 #define fir first
 #define sec second
-#define MAXN 300001
+#define MAXN 100001
 #define MAXL 20
 #define mod 1000000009
 
@@ -17,19 +17,32 @@ vector<int> seg(4 * MAXN);
 vector<int> lazy(4 * MAXN);
 vector<int> v(MAXN);
 
+int single(int x)
+{
+  return x;
+}
+int neutral()
+{
+  return 0;
+}
+int merge(int a, int b)
+{
+  return a + b;
+}
 void add(int i, int l, int r, int diff)
 {
   seg[i] += (r - l + 1) * diff;
   if (l != r)
   {
-    lazy[l << 1] += diff;
-    lazy[(l << 1) | 1] += diff;
+    lazy[i << 1] += diff;
+    lazy[(i << 1) | 1] += diff;
   }
+  lazy[i] = 0;
 }
 void update(int i, int l, int r, int ql, int qr, int diff)
 {
-  if (lazy[i] != 0)
-    add(i, l, r, lazy[i]), lazy[i] = 0;
+  if (lazy[i])
+    add(i, l, r, lazy[i]);
   if (l > r || l > qr || r < ql)
     return;
   if (l >= ql && r <= qr)
@@ -40,48 +53,51 @@ void update(int i, int l, int r, int ql, int qr, int diff)
   int mid = (l + r) >> 1;
   update(i << 1, l, mid, ql, qr, diff);
   update((i << 1) | 1, mid + 1, r, ql, qr, diff);
-  seg[i] = seg[i << 1] + seg[(i << 1) | 1] ;
+  seg[i] = merge(seg[i << 1], seg[(i << 1) | 1]);
 }
 int query(int l, int r, int ql, int qr, int i)
 {
-  int mid = (l + r) >> 1;
-  if (lazy[i] != 0)
-    add(i, l, r, lazy[i]), lazy[i] = 0;
+  if (lazy[i])
+    add(i, l, r, lazy[i]);
   if (l > r || l > qr || r < ql)
-    return 0;
+    return neutral();
   if (l >= ql && r <= qr)
     return seg[i];
-  return query(l, mid, ql, qr, i << 1) + query(mid + 1, r, ql, qr, (i << 1) | 1) ;
+  int mid = (l + r) >> 1;
+  return merge(query(l, mid, ql, qr, i << 1), query(mid + 1, r, ql, qr, (i << 1) | 1));
 }
 void build(int l, int r, int i)
 {
   if (l == r)
   {
-    seg[i] = v[l];
+    seg[i] = single(v[l]);
     return;
   }
   int mid = (l + r) >> 1;
   build(l, mid, i << 1);
   build(mid + 1, r, (i << 1) | 1);
-  seg[i] = seg[i << 1] + seg[(i << 1) | 1] ;
+  seg[i] = merge(seg[i << 1], seg[(i << 1) | 1]);
 }
 signed main()
 {
-  int n;
-  cin >> n;
-  for (int i = 0; i < n; i++)
-    cin >> v[i];
+  int n, q;
+  cin >> n >> q;
   build(0, n - 1, 1);
-  int q;
-  cin >> q;
   while (q--)
   {
-    int l, r;
-    char t;
-    cin >> t >> l >> r;
-    if (t == 'Q')
-      cout << query(0, n - 1, l, r, 1) << endl;
+    int t;
+    cin >> t;
+    if (t == 2)
+    {
+      int l;
+      cin >> l;
+      cout << query(0, n - 1, l, l, 1) << endl;
+    }
     else
-      update(1, 0, n - 1, l, l, r - v[l]), v[l] += (r - v[l]);
+    {
+      int l, r, v;
+      cin >> l >> r >> v;
+      update(1, 0, n - 1, l, r - 1, v);
+    }
   }
 }
