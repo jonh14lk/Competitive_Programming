4c65f9ebf83f62235abfd6e1748860820ec30cec
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-11-18 11:30:36 -0300

    update bfs

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -1,60 +1,54 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
-#define MAXN 10000
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-int n , m ;
-int visited [MAXN] ;
-vector <int> adj_list [MAXN] ;
+#define int long long int
+#define pb push_back
+#define pi pair<int, int>
+#define pii pair<int, pi>
+#define fir first
+#define sec second
+#define DEBUG 1
+#define MAXN 1001
+#define mod 1000000007
 
-void bfs (int x)
+int n, m;
+vector<int> adj[MAXN];
+bool visited[MAXN];
+
+void bfs(int s)
 {
-    queue <int> q ;
-    q.push(x) ;
-
-    visited[x] = 1 ; 
-
-    while(!q.empty())
-    { 
-        int v = q.front() ;
-        q.pop() ; 
-
-        for(int i = 0 ; i < adj_list[v].size() ;i++)
-        {
-            int u = adj_list[v][i] ;
-
-            if(visited[u] == -1)
-            {
-                visited[u] = visited[v] ;
-                q.push(u) ;
-            }
-        }
-    }
+  queue<int> q;
+  q.push(s);
+  while (!q.empty())
+  {
+    int v = q.front();
+    q.pop();
+    if (visited[v])
+      continue;
+    visited[v] = true;
+    for (auto const &u : adj[v])
+      if (!visited[u])
+        q.push(u);
+  }
 }
-void initialize ()
+signed main()
 {
-    for (int i = 1 ; i <= n ; i++) 
-    {
-        visited[i] = -1 ; 
-    }
-}
-int main()
-{
-    int a , b ;
-
-    cin >> n >> m ;
-
-    initialize();
-
-    for (int i = 1 ; i <= m ; i++)
-    {
-        cin >> a >> b ;
-
-        adj_list[a].push_back(b) ;
-        adj_list[b].push_back(a) ;
-    }
-    
-    bfs(1) ;
-
-    return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin >> n >> m;
+  for (int i = 0; i < m; i++)
+  {
+    int a, b, c;
+    cin >> a >> b >> c;
+    a--, b--;
+    adj[a].pb(b);
+    adj[b].pb(a);
+  }
+  bfs(0);
 }
