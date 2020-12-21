2a836e608037b945525ea405067923b2d636bbf7
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-12-21 19:59:04 -0300

    change my hld template

--- Structures/hld.cpp
+++ Structures/hld.cpp
@@ -1,104 +1,161 @@
-#include <bits/stdc++.h> // hld com seg de maximo
+//https://codeforces.com/contest/343/problem/D
+#include <bits/stdc++.h>
 using namespace std;
 
-#define PI acos(-1)
 #define int long long int
 #define pb push_back
-#define mp make_pair
-#define pi pair<string, int>
-#define pii pair<int, pi>
+#define pi pair<int, int>
+#define pii pair<pi, int>
 #define fir first
 #define sec second
-#define MAXN 200001
-#define MAXL 20
-#define mod 998244353
+#define DEBUG 0
+#define MAXN 500001
+#define mod 1000000007
 
-int n, cur_pos;
-int seg[4 * MAXN];
+int n, q;
 vector<int> adj[MAXN];
-vector<int> parent, depth, heavy, head, pos;
 
-int seg_query(int i, int l, int r, int ql, int qr)
+namespace seg
 {
-  if (l >= ql && r <= qr)
-    return seg[i];
-  if (l > qr || r < ql)
-    return -1;
-  int mid = (l + r) / 2;
-  return max(seg_query(2 * i, l, mid, ql, qr), seg_query((2 * i) + 1, mid + 1, r, ql, qr));
-}
-void seg_update(int i, int l, int r, int pos, int x)
-{
-  if (l == r)
-    seg[i] = x;
-  else
+  int seg[4 * MAXN];
+  int lazy[4 * MAXN];
+
+  int single(int x)
   {
-    int mid = (l + r) / 2;
-    if (pos <= mid)
-      seg_update(2 * i, l, mid, pos, x);
-    else
-      seg_update((2 * i) + 1, mid + 1, r, pos, x);
-    seg[i] = max(seg[2 * i], seg[(2 * i) + 1]);
+    return x;
   }
-}
-int dfs(int s)
-{
-  int size = 1, max_c_size = 0;
-  for (auto const &c : adj[s])
+  int neutral()
   {
-    if (c != parent[s])
+    return 0;
+  }
+  int merge(int a, int b)
+  {
+    return a + b;
+  }
+  void add(int i, int l, int r, int diff)
+  {
+    seg[i] = (r - l + 1) * diff;
+    if (l != r)
     {
-      parent[c] = s;
-      depth[c] = depth[s] + 1;
-      int c_size = dfs(c);
-      size += c_size;
-      if (c_size > max_c_size)
-        max_c_size = c_size, heavy[s] = c;
+      lazy[i << 1] = diff;
+      lazy[(i << 1) | 1] = diff;
+    }
+    lazy[i] = -1;
+  }
+  void update(int i, int l, int r, int ql, int qr, int diff)
+  {
+    if (lazy[i] != -1)
+      add(i, l, r, lazy[i]);
+    if (l > r || l > qr || r < ql)
+      return;
+    if (l >= ql && r <= qr)
+    {
+      add(i, l, r, diff);
+      return;
+    }
+    int mid = (l + r) >> 1;
+    update(i << 1, l, mid, ql, qr, diff);
+    update((i << 1) | 1, mid + 1, r, ql, qr, diff);
+    seg[i] = merge(seg[i << 1], seg[(i << 1) | 1]);
+  }
+  int query(int l, int r, int ql, int qr, int i)
+  {
+    if (lazy[i] != -1)
+      add(i, l, r, lazy[i]);
+    if (l > r || l > qr || r < ql)
+      return neutral();
+    if (l >= ql && r <= qr)
+      return seg[i];
+    int mid = (l + r) >> 1;
+    return merge(query(l, mid, ql, qr, i << 1), query(mid + 1, r, ql, qr, (i << 1) | 1));
+  }
+} // namespace seg
+namespace hld
+{
+  int cur_pos;
+  vector<int> parent, depth, heavy, head, pos, sz;
+
+  int dfs(int s)
+  {
+    int size = 1, max_c_size = 0;
+    for (auto const &c : adj[s])
+    {
+      if (c != parent[s])
+      {
+        parent[c] = s;
+        depth[c] = depth[s] + 1;
+        int c_size = dfs(c);
+        size += c_size;
+        if (c_size > max_c_size)
+          max_c_size = c_size, heavy[s] = c;
+      }
+    }
+    return sz[s] = size;
+  }
+  void decompose(int s, int h)
+  {
+    head[s] = h;
+    pos[s] = cur_pos++;
+    if (heavy[s] != -1)
+      decompose(heavy[s], h);
+    for (int c : adj[s])
+    {
+      if (c != parent[s] && c != heavy[s])
+        decompose(c, c);
     }
   }
-  return size;
-}
-void decompose(int s, int h)
-{
-  head[s] = h;
-  pos[s] = cur_pos++;
-  if (heavy[s] != -1)
-    decompose(heavy[s], h);
-  for (int c : adj[s])
+  void init()
   {
-    if (c != parent[s] && c != heavy[s])
-      decompose(c, c);
+    memset(seg::lazy, -1, sizeof(seg::lazy));
+    parent.assign(MAXN, -1);
+    depth.assign(MAXN, -1);
+    heavy.assign(MAXN, -1);
+    head.assign(MAXN, -1);
+    pos.assign(MAXN, -1);
+    sz.assign(MAXN, 1);
+    cur_pos = 0;
+    dfs(0);
+    decompose(0, 0);
+    for (int i = 0; i < 4 * n; i++)
+      seg::lazy[i] = -1;
   }
-}
-void init()
-{
-  parent.assign(MAXN, -1);
-  depth.assign(MAXN, -1);
-  heavy.assign(MAXN, -1);
-  head.assign(MAXN, -1);
-  pos.assign(MAXN, -1);
-  cur_pos = 0;
-  dfs(0);
-  decompose(0, 0);
-  for (int i = 0; i < n; i++)
-    seg_update(1, 0, n - 1, pos[i], i);
-}
-int query(int a, int b)
-{
-  int res = 0;
-  for (; head[a] != head[b]; b = parent[head[b]])
+  int query_path(int a, int b)
   {
-    if (depth[head[a]] > depth[head[b]])
+    int res = 0;
+    for (; head[a] != head[b]; b = parent[head[b]])
+    {
+      if (depth[head[a]] > depth[head[b]])
+        swap(a, b);
+      int cur_heavy_path_max = seg::query(0, n - 1, pos[head[b]], pos[b], 1);
+      res += cur_heavy_path_max;
+    }
+    if (depth[a] > depth[b])
       swap(a, b);
-    int cur_heavy_path_max = seg_query(1, 0, n - 1, pos[head[b]], pos[b]);
-    res = max(res, cur_heavy_path_max);
+    int last_heavy_path_max = seg::query(0, n - 1, pos[a], pos[b], 1);
+    res += last_heavy_path_max;
+    return res;
   }
-  if (depth[a] > depth[b])
-    swap(a, b);
-  int last_heavy_path_max = seg_query(1, 0, n - 1, pos[a], pos[b]);
-  res = max(res, last_heavy_path_max);
-  return res;
-}
+  void update_path(int a, int b, int x)
+  {
+    for (; head[a] != head[b]; b = parent[head[b]])
+    {
+      if (depth[head[a]] > depth[head[b]])
+        swap(a, b);
+      seg::update(1, 0, n - 1, pos[head[b]], pos[b], x);
+    }
+    if (depth[a] > depth[b])
+      swap(a, b);
+    seg::update(1, 0, n - 1, pos[a], pos[b], x);
+  }
+  void update_subtree(int a, int x)
+  {
+    seg::update(1, 0, n - 1, pos[a], pos[a] + sz[a] - 1, x);
+  }
+  void query_subtree(int a, int x)
+  {
+    seg::query(0, n - 1, pos[a], pos[a] + sz[a] - 1, 1);
+  }
+} // namespace hld
 signed main()
 {
   cin >> n;
@@ -110,15 +167,25 @@ signed main()
     adj[a].pb(b);
     adj[b].pb(a);
   }
-  init();
-  int q;
+  hld::init();
   cin >> q;
   while (q--)
   {
     int a, b;
     cin >> a >> b;
-    a--, b--;
-    cout << query(a, b) + 1 << endl;
+    b--;
+    if (a == 1)
+    {
+      hld::update_subtree(b, 1);
+    }
+    if (a == 2)
+    {
+      hld::update_path(0, b, 0);
+    }
+    if (a == 3)
+    {
+      cout << hld::query_path(b, b) << endl;
+    }
   }
   return 0;
 }
