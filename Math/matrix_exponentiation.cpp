ea97e16a468981f7850e90a6b6711ae499409930
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2021-01-13 12:01:26 -0300

    update matrix exponentiation code

--- Math/matrix_exponentiation.cpp
+++ Math/matrix_exponentiation.cpp
@@ -10,60 +10,75 @@
 // dai calculamos o n-ésimo termo da sequencia de fibonacci mod (10^9 + 7) em O(log(n))
 
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define PI acos(-1)
 #define pb push_back
-#define mp make_pair
 #define int long long int
 #define pi pair<int, int>
 #define pii pair<pi, int>
 #define fir first
 #define sec second
-#define MAXN 100001
-#define MAXL 20
-#define INF 200001
+#define DEBUG 0
+#define MAXN 201
 #define mod 1000000007
 
-const int n = 2;
-vector<vector<int>> ans(n, vector<int>(n));
+namespace matrix
+{
+  vector<vector<int>> ans;
 
-vector<vector<int>> multiply(vector<vector<int>> a, vector<vector<int>> b)
-{
-  vector<vector<int>> res(n, vector<int>(n));
-  for (int i = 0; i < n; i++)
+  int multi(int x, int y)
   {
-    for (int j = 0; j < n; j++)
+    return (x * y) % mod;
+  }
+  int sum(int a, int b)
+  {
+    return (a + b >= mod) ? a + b - mod : a + b;
+  }
+  vector<vector<int>> multiply(vector<vector<int>> a, vector<vector<int>> b)
+  {
+    vector<vector<int>> res(a[0].size(), vector<int>(b[0].size()));
+    for (int i = 0; i < a.size(); i++)
     {
-      res[i][j] = 0;
-      for (int k = 0; k < n; k++)
-        res[i][j] = (res[i][j] + (a[i][k] * b[k][j]) % mod) % mod;
+      for (int j = 0; j < b[0].size(); j++)
+      {
+        res[i][j] = 0;
+        for (int k = 0; k < a[0].size(); k++)
+          res[i][j] = sum(res[i][j], multi(a[i][k], b[k][j]));
+      }
     }
+    return res;
   }
-  return res;
-}
-vector<vector<int>> expo(vector<vector<int>> mat, int m)
-{
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      ans[i][j] = (i == j);
-  while (m > 0)
+  vector<vector<int>> expo(vector<vector<int>> mat, int m)
   {
-    if (m & 1)
-      ans = multiply(ans, mat);
-    m = m / 2;
-    mat = multiply(mat, mat);
+    ans = vector<vector<int>>(mat.size(), vector<int>(mat[0].size()));
+    for (int i = 0; i < mat.size(); i++)
+      for (int j = 0; j < mat[0].size(); j++)
+        ans[i][j] = (i == j);
+    while (m > 0)
+    {
+      if (m & 1)
+        ans = multiply(ans, mat);
+      m = m / 2;
+      mat = multiply(mat, mat);
+    }
+    return ans;
   }
-  return ans;
 }
 signed main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int m;
-  cin >> m;
+  int n;
+  cin >> n;
   vector<vector<int>> mat = {{1, 1}, {1, 0}};
-  vector<vector<int>> ans = expo(mat, m - 1);
-  cout << ans[0][0] + ans[0][1] << endl;
+  vector<vector<int>> ans = matrix::expo(mat, n);
+  cout << ans[0][1] << endl;
   return 0;
 }
