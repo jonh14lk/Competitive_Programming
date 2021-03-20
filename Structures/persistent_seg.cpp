757ddb4288016723e1c96022b5daece2b6b1ab35
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2021-03-20 12:27:33 -0300

    now persistent segtree have lazy prop

--- Structures/persistent_seg.cpp
+++ Structures/persistent_seg.cpp
@@ -10,121 +10,149 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 #define int long long int
 #define pb push_back
 #define pi pair<int, int>
-#define pii pair<int, pi>
+#define pii pair<pi, int>
 #define fir first
 #define sec second
-#define MAXN 300005
+#define MAXN 100003
 #define mod 1000000007
 
-int n;
-vector<int> v;
+int v[MAXN];
 
-namespace seg_pers
+namespace seg
 {
-  struct item
+  struct node
   {
-    int sum, l, r;
+    int item, lazy, lazy_status, l, r;
   };
 
-  int time;
-  vector<item> seg;
-  vector<int> parents;
+  int cnt;
+  node seg[500 * MAXN];
+  vector<int> roots;
 
-  void update(int i, int l, int r, int q, int x)
+  int neutral()
   {
-    seg.pb({0, 0, 0});
-    int curr = ++time;
-    if (l == r)
-    {
-      seg[curr].sum = x;
+    return 0;
+  }
+  int merge(int a, int b)
+  {
+    return a + b;
+  }
+  int newleaf(int vv)
+  {
+    int p = ++cnt;
+    seg[p].l = 0;
+    seg[p].r = 0;
+    seg[p].lazy = 0;
+    seg[p].lazy_status = 0;
+    seg[p].item = vv;
+    return p;
+  }
+  int newparent(int l, int r)
+  {
+    int p = ++cnt;
+    seg[p].l = l;
+    seg[p].r = r;
+    seg[p].lazy = 0;
+    seg[p].lazy_status = 0;
+    seg[p].item = merge(seg[seg[p].l].item, seg[seg[p].r].item);
+    return p;
+  }
+  int newkid(int i, int diff, int l, int r)
+  {
+    int p = ++cnt;
+    seg[p].l = seg[i].l;
+    seg[p].r = seg[i].r;
+    seg[p].lazy = seg[i].lazy + diff;
+    seg[p].lazy_status = 1;
+    seg[p].item = seg[i].item + ((r - l + 1) * diff);
+    return p;
+  }
+  void add(int i, int l, int r)
+  {
+    if (!seg[i].lazy_status)
       return;
+    if (l != r)
+    {
+      int mid = (l + r) >> 1;
+      seg[i].l = newkid(seg[i].l, seg[i].lazy, l, mid);
+      seg[i].r = newkid(seg[i].r, seg[i].lazy, mid + 1, r);
     }
+    seg[i].lazy = 0;
+    seg[i].lazy_status = 0;
+  }
+  int update(int i, int l, int r, int ql, int qr, int diff)
+  {
+    if (l > r || l > qr || r < ql)
+      return i;
+    if (l >= ql && r <= qr)
+      return newkid(i, diff, l, r);
+    add(i, l, r);
     int mid = (l + r) >> 1;
-    if (q <= mid)
-    {
-      seg[curr].l = time + 1;
-      seg[curr].r = seg[i].r;
-      update(seg[i].l, l, mid, q, x);
-    }
-    else
-    {
-      seg[curr].l = seg[i].l;
-      seg[curr].r = time + 1;
-      update(seg[i].r, mid + 1, r, q, x);
-    }
-    seg[curr].sum = seg[seg[curr].l].sum + seg[seg[curr].r].sum;
+    return newparent(update(seg[i].l, l, mid, ql, qr, diff), update(seg[i].r, mid + 1, r, ql, qr, diff));
   }
   int query(int l, int r, int ql, int qr, int i)
   {
-    int mid = (l + r) >> 1;
     if (l > r || l > qr || r < ql)
-      return 0;
+      return neutral();
     if (l >= ql && r <= qr)
-      return seg[i].sum;
-    return query(l, mid, ql, qr, seg[i].l) + query(mid + 1, r, ql, qr, seg[i].r);
+      return seg[i].item;
+    add(i, l, r);
+    int mid = (l + r) >> 1;
+    return merge(query(l, mid, ql, qr, seg[i].l), query(mid + 1, r, ql, qr, seg[i].r));
   }
-  void build(int l, int r, int i)
+  int build(int l, int r)
   {
     if (l == r)
-    {
-      seg[i].sum = v[l];
-      return;
-    }
+      return newleaf(v[l]);
     int mid = (l + r) >> 1;
-    seg.pb({0, 0, 0});
-    seg[i].l = ++time;
-    build(l, mid, seg[i].l);
-    seg.pb({0, 0, 0});
-    seg[i].r = ++time;
-    build(mid + 1, r, seg[i].r);
-    seg[i].sum = seg[seg[i].l].sum + seg[seg[i].r].sum;
-  }
-  void init()
-  {
-    time = 1;
-    seg.resize(2);
-    parents.resize(1);
-    parents[0] = 1;
+    return newparent(build(l, mid), build(mid + 1, r));
   }
 }
 signed main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  cin >> n;
-  v.resize(n);
+  int n, q;
+  cin >> n >> q;
   for (int i = 0; i < n; i++)
     cin >> v[i];
-  seg_pers::init();
-  seg_pers::build(0, n - 1, 1);
-  int q;
-  cin >> q;
+  int root = seg::build(0, n - 1);
+  seg::roots.pb(root);
   while (q--)
   {
-    int t;
+    char t;
     cin >> t;
-    if (t == 1) // update
+    if (t == 'C')
     {
-      int i, x;
-      cin >> i >> x;
-      i--;
-      seg_pers::parents.pb(seg_pers::time + 1);
-      seg_pers::update(seg_pers::parents[seg_pers::parents.size() - 2], 0, n - 1, i, x);
+      int l, r, d;
+      cin >> l >> r >> d;
+      l--, r--;
+      int root = seg::update(seg::roots.back(), 0, n - 1, l, r, d);
+      seg::roots.pb(root);
     }
-    else if (t == 2) // query
+    else if (t == 'Q')
     {
       int l, r;
       cin >> l >> r;
       l--, r--;
-      seg_pers::parents.pb(seg_pers::parents.back());
-      cout << seg_pers::query(0, n - 1, l, r, seg_pers::parents.back()) << endl;
+      cout << seg::query(0, n - 1, l, r, seg::roots.back()) << endl;
     }
-    else // rollback
+    else if (t == 'H')
     {
-      int k;
-      cin >> k;
-      seg_pers::parents.pb(seg_pers::parents[k]);
+      int l, r, d;
+      cin >> l >> r >> d;
+      l--, r--;
+      cout << seg::query(0, n - 1, l, r, seg::roots[d]) << endl;
+    }
+    else
+    {
+      int d;
+      cin >> d;
+      while (seg::roots.size() > d + 1)
+        seg::roots.pop_back();
     }
   }
   return 0;
 }
+// https://www.spoj.com/problems/TTM/
+// rollback segtree to a time stamp t
