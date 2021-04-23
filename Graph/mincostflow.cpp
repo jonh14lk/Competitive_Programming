464cca1b726ea5415a499b6094cbdea3808e7a16
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2021-04-22 21:32:18 -0300

    updates on mincostflow code

--- Graph/mincostflow.cpp
+++ Graph/mincostflow.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define int long long int
 #define pb push_back
@@ -7,96 +13,105 @@ using namespace std;
 #define pii pair<int, pi>
 #define fir first
 #define sec second
-#define DEBUG true
 #define MAXN 301
+#define mod 1000000007
 #define INF 1e9
 
-int n, source, destiny;
-vector<int> adj[MAXN];
-int capacity[MAXN][MAXN];
-int cost[MAXN][MAXN];
-vector<int> dist;
-vector<int> parent;
-vector<bool> in_queue;
-
-void add_edge(int a, int b, int c, int d)
+namespace mcf
 {
-  adj[a].pb(b); // aresta normal
-  capacity[a][b] = c;
-  cost[a][b] = d;
-  adj[b].pb(a); // aresta do grafo residual
-  capacity[b][a] = 0;
-  cost[b][a] = -d;
-}
-bool dijkstra(int s) // rodando o dijkstra, terei o caminho de custo minimo
-{                    // que eu consigo passando pelas arestas que possuem capacidade > 0
-  dist.assign(MAXN, INF);
-  parent.assign(MAXN, -1);
-  in_queue.assign(MAXN, false);
-  dist[s] = 0;
-  queue<int> q;
-  q.push(s);
-  while (!q.empty())
+  struct edge
   {
-    int u = q.front();
-    q.pop();
-    in_queue[u] = false;
-    for (auto const &v : adj[u])
+    int to, capacity, cost, res;
+  };
+
+  int source, destiny;
+  vector<edge> adj[MAXN];
+  vector<int> dist;
+  vector<int> parent;
+  vector<int> edge_index;
+  vector<bool> in_queue;
+
+  void add_edge(int a, int b, int c, int d)
+  {
+    adj[a].pb({b, c, d, (int)adj[b].size()});      // aresta normal
+    adj[b].pb({a, 0, -d, (int)adj[a].size() - 1}); // aresta do grafo residual
+  }
+  bool dijkstra(int s) // rodando o dijkstra, terei o caminho de custo minimo
+  {                    // que eu consigo passando pelas arestas que possuem capacidade > 0
+    dist.assign(MAXN, INF);
+    parent.assign(MAXN, -1);
+    edge_index.assign(MAXN, -1);
+    in_queue.assign(MAXN, false);
+    dist[s] = 0;
+    queue<int> q;
+    q.push(s);
+    while (!q.empty())
     {
-      if (capacity[u][v] && dist[v] > dist[u] + cost[u][v])
+      int u = q.front(), idx = 0;
+      q.pop();
+      in_queue[u] = false;
+      for (auto const &v : adj[u])
       {
-        dist[v] = dist[u] + cost[u][v];
-        parent[v] = u;
-        if (!in_queue[v])
+        if (v.capacity && dist[v.to] > dist[u] + v.cost)
         {
-          in_queue[v] = true;
-          q.push(v);
+          dist[v.to] = dist[u] + v.cost;
+          parent[v.to] = u;
+          edge_index[v.to] = idx;
+          if (!in_queue[v.to])
+          {
+            in_queue[v.to] = true;
+            q.push(v.to);
+          }
         }
+        idx++;
       }
     }
+    return dist[destiny] != INF; // se eu cheguei em destiny por esse caminho, ainda posso passar fluxo
   }
-  return dist[destiny] != INF; // se eu cheguei em destiny por esse caminho, ainda posso passar fluxo
-}
-int min_cost_flow()
-{
-  int flow = 0, cost = 0;
-  while (dijkstra(source)) // rodo um dijkstra para saber qual o caminho que irei agora
+  int get_cost()
   {
-    int curr_flow = INF, curr = destiny;
-    while (curr != source) // com isso, vou percorrendo o caminho encontrado para achar a aresta "gargalo"
+    int flow = 0, cost = 0;
+    while (dijkstra(source)) // rodo um dijkstra para saber qual o caminho que irei agora
     {
-      curr_flow = min(curr_flow, capacity[parent[curr]][curr]);
-      curr = parent[curr];
+      int curr_flow = INF, curr = destiny;
+      while (curr != source) // com isso, vou percorrendo o caminho encontrado para achar a aresta "gargalo"
+      {
+        int p = parent[curr];
+        curr_flow = min(curr_flow, adj[p][edge_index[curr]].capacity);
+        curr = p;
+      }
+      flow += curr_flow;                 // fluxo que eu posso passar por esse caminho = custo da aresta "gargalo"
+      cost += curr_flow * dist[destiny]; // quanto eu gasto para passar esse fluxo no caminho encontrado
+      curr = destiny;
+      while (curr != source) // apos achar a aresta gargalo, passamos o fluxo pelo caminho encontrado
+      {
+        int p = parent[curr];
+        int res_idx = adj[p][edge_index[curr]].res;
+        adj[p][edge_index[curr]].capacity -= curr_flow;
+        adj[curr][res_idx].capacity += curr_flow;
+        curr = p;
+      }
     }
-    flow += curr_flow;                 // fluxo que eu posso passar por esse caminho = custo da aresta "gargalo"
-    cost += curr_flow * dist[destiny]; // quanto eu gasto para passar esse fluxo no caminho encontrado
-    curr = destiny;
-    while (curr != source) // apos achar a aresta gargalo, passamos o fluxo pelo caminho encontrado
-    {
-      capacity[parent[curr]][curr] -= curr_flow;
-      capacity[curr][parent[curr]] += curr_flow;
-      curr = parent[curr];
-    }
+    return cost; // ao final temos a resposta :)
   }
-  return cost; // ao final temos a resposta :)
-}
+} // namespace mcf
 signed main()
 {
   int n;
   cin >> n;
   int v[n][n];
-  source = 0, destiny = (2 * n) + 1;
+  mcf::source = 0, mcf::destiny = (2 * n) + 1;
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
     {
       cin >> v[i][j];
-      add_edge(i + 1, j + n + 1, 1, v[i][j]);
+      mcf::add_edge(i + 1, j + n + 1, 1, v[i][j]);
     }
   }
   for (int i = 1; i <= n; i++)
-    add_edge(source, i, 1, 0);
+    mcf::add_edge(mcf::source, i, 1, 0);
   for (int i = n + 1; i <= n + n; i++)
-    add_edge(i, destiny, 1, 0);
-  cout << min_cost_flow() << endl;
+    mcf::add_edge(i, mcf::destiny, 1, 0);
+  cout << mcf::get_cost << endl;
 }
