23a236f21281c26f22da9629a758117fa80b4b2b
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2021-05-10 17:09:36 -0300

    calculating execution time

---
+++ Utils/execution_time.cpp
@@ -0,0 +1,36 @@
+// https://codeforces.com/blog/entry/57647
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int long long int
+#define pb push_back
+#define pi pair<int, int>
+#define pii pair<int, pi>
+#define fir first
+#define sec second
+#define MAXN 300005
+#define mod 1000000007
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  // just call clock in the beginning
+  clock_t time = clock();
+
+  // ...
+  // ...
+  // some code here ...
+  // ...
+  // ...
+
+  // execution time:
+  cout << setprecision(3) << fixed << (double)(clock() - time) / CLOCKS_PER_SEC << endl;
+  return 0;
+}
