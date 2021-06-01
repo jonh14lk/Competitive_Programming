8c79583a0615072046ae713d31103f3e09eb150a
jonh14lk <jvaaa@ic.ufal.br>
2021-06-01 10:01:55 -0300

    Examples of problems with bitmasks

--- Dynamic programming and common problems/bitmasks.cpp
+++ Dynamic programming and common problems/bitmasks.cpp
@@ -21,6 +21,10 @@ signed main()
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   int n, mask;
+  vector<int> masks;
+
+  // quantidade de bits setados na mask
+  cout << __builtin_popcount(mask) << endl;
 
   // para printar o valor do bit i
   for (int i = 0; i < n; i++)
@@ -29,6 +33,7 @@ signed main()
 
   // quando eh necessario percorrer todas as submasks ate (1 << n)
   // e fazer algo com todas as submasks dessa mask
+  // util em problemas de dp com mask por exemplo
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < (1 << n); j++)
@@ -48,12 +53,28 @@ signed main()
 
   // quando eh necessario percorrer todas as submasks ate (1 << n)
   // e fazer algo com todas as submasks dessa mask O(3^n)
+  // util em problemas de dp com mask por exemplo
   for (int m = 0; m < (1 << n); m++)
   {
     for (int s = m; s; s = (s - 1) & m)
     {
       // alguma coisa aqui sabendo que mask s eh uma submask de m
     }
+  }
+
+  // comprimindo as masks de um vector baseada em uma mask qualquer
+  for (int i = 0; i < masks.size(); i++)
+  {
+    int compressed = 0, curr_bit = 0;
+    for (int j = 0; j < n; j++)
+    {
+      if (!(mask & (1LL << j)))
+        continue;
+      if (masks[i] & (1LL << j))
+        compressed |= (1LL << curr_bit);
+      curr_bit++;
+    }
+    // alguma coisa sabendo que a mask compressed eh a mask comprimida da mask atual
   }
   return 0;
 }
