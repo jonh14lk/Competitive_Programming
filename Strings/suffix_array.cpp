f954b96e9725d39c233ca53d90ea0078a52f6cb6
jonh14lk <jvaaa@ic.ufal.br>
2021-07-07 09:44:44 -0300

    Updating Suffix array template

--- Strings/suffix_array.cpp
+++ Strings/suffix_array.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
-#define PI acos(-1)
-#define pb push_back
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 #define int long long int
-#define mp make_pair
+#define pb push_back
 #define pi pair<int, int>
 #define pii pair<pi, int>
 #define pci pair<char, int>
 #define fir first
 #define sec second
-#define MAXN 100001
-#define MAXL 20
+#define MAXN 50005
 #define mod 1000000007
 
 void get_suf(string s)
@@ -21,7 +24,7 @@ void get_suf(string s)
   vector<int> p(n), c(n);
   vector<pci> a(n);
   for (int i = 0; i < n; i++)
-    a[i] = mp(s[i], i);
+    a[i] = {s[i], i};
   sort(a.begin(), a.end());
   for (int i = 0; i < n; i++)
     p[i] = a[i].sec;
@@ -33,7 +36,7 @@ void get_suf(string s)
   {
     vector<pii> v(n);
     for (int i = 0; i < n; i++)
-      v[i] = mp(mp(c[i], c[(i + (1 << k)) % n]), i);
+      v[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
     sort(v.begin(), v.end());
     for (int i = 0; i < n; i++)
       p[i] = v[i].sec;
@@ -48,6 +51,8 @@ void get_suf(string s)
 }
 signed main()
 {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   string s;
   cin >> s;
   get_suf(s);
