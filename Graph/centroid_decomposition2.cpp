95f6488d20afa546a790ecd372d8e0f7a99e9798
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2021-09-27 20:18:20 -0300

    amazing centroid decomposition problem

---
+++ Graph/centroid_decomposition2.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int long long int
+#define pb push_back
+#define pi pair<int, int>
+#define pii pair<int, pi>
+#define fir first
+#define sec second
+#define MAXN 50005
+#define mod 1000000007
+
+int n, k, resp;
+vector<int> adj[MAXN];
+gp_hash_table<int, int> cnt;
+
+namespace cd
+{
+  int sz;
+  vector<int> adjl[MAXN];
+  vector<int> father, subtree_size;
+  vector<bool> visited;
+
+  void dfs(int s, int f)
+  {
+    sz++;
+    subtree_size[s] = 1;
+    for (auto const &v : adj[s])
+    {
+      if (v != f && !visited[v])
+      {
+        dfs(v, s);
+        subtree_size[s] += subtree_size[v];
+      }
+    }
+  }
+  int getCentroid(int s, int f)
+  {
+    bool is_centroid = true;
+    int heaviest_child = -1;
+    for (auto const &v : adj[s])
+    {
+      if (v != f && !visited[v])
+      {
+        if (subtree_size[v] > sz / 2)
+          is_centroid = false;
+        if (heaviest_child == -1 || subtree_size[v] > subtree_size[heaviest_child])
+          heaviest_child = v;
+      }
+    }
+    return (is_centroid && sz - subtree_size[s] <= sz / 2) ? s : getCentroid(heaviest_child, s);
+  }
+  void dfs2(int s, int f, int d)
+  {
+    cnt[d]++;
+    for (auto const &v : adj[s])
+      if (v != f && !visited[v])
+        dfs2(v, s, d + 1);
+  }
+  int solve(int s)
+  {
+    gp_hash_table<int, int> tot;
+    int ans = 0;
+    for (auto const &v : adj[s])
+    {
+      if (visited[v])
+        continue;
+      cnt.clear();
+      dfs2(v, s, 1);
+      for (int i = 1, j = k - 1; i < k; i++, j--)
+        ans += (cnt[i] * tot[j]);
+      for (auto const &i : cnt)
+        tot[i.fir] += i.sec;
+    }
+    return ans + tot[k];
+  }
+  int decompose_tree(int s)
+  {
+    sz = 0;
+    dfs(s, s);
+    int cend_tree = getCentroid(s, s);
+    visited[cend_tree] = true;
+    resp += solve(cend_tree);
+    for (auto const &v : adj[cend_tree])
+    {
+      if (!visited[v])
+      {
+        int cend_subtree = decompose_tree(v);
+        adjl[cend_tree].pb(cend_subtree);
+        adjl[cend_subtree].pb(cend_tree);
+        father[cend_subtree] = cend_tree;
+      }
+    }
+    return cend_tree;
+  }
+  void init()
+  {
+    subtree_size.resize(n);
+    visited.resize(n);
+    father.assign(n, -1);
+    decompose_tree(0);
+  }
+}
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin >> n >> k;
+  for (int i = 0; i < n - 1; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    adj[a].pb(b);
+    adj[b].pb(a);
+  }
+  cd::init();
+  cout << resp << endl;
+  return 0;
+}
+// https://codeforces.com/contest/161/problem/D
+// durante a decomposição
+// pega o centroid atual e resolve o problema pra ele
+// isso eh:
+// para cada centroid que eu achei, devo contar quantos caminhos
+// de tamanho k passam por esse centroid
+// somando todas essas respostas, a gente tem a resposta final
