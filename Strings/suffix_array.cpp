b279c07189ec553db34201b859abb2640f13b532
jonh14lk <jvaaa@ic.ufal.br>
2022-08-24 11:54:32 -0300

    changing suffix array

--- Strings/suffix_array.cpp
+++ Strings/suffix_array.cpp
@@ -8,53 +8,120 @@ template <class T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define int long long int
+#define endl '\n'
 #define pb push_back
 #define pi pair<int, int>
 #define pii pair<pi, int>
 #define pci pair<char, int>
 #define fir first
 #define sec second
-#define MAXN 50005
+#define MAXN 500001
 #define mod 1000000007
 
-void get_suf(string s)
+struct suffix_array
 {
-  s += '$';
-  int n = s.size();
-  vector<int> p(n), c(n);
-  vector<pci> a(n);
-  for (int i = 0; i < n; i++)
-    a[i] = {s[i], i};
-  sort(a.begin(), a.end());
-  for (int i = 0; i < n; i++)
-    p[i] = a[i].sec;
-  c[p[0]] = 0;
-  for (int i = 1; i < n; i++)
-    (a[i].fir == a[i - 1].fir) ? c[p[i]] = c[p[i - 1]] : c[p[i]] = c[p[i - 1]] + 1;
-  int k = 0;
-  while ((1 << k) < n)
+  int n, k;
+  string s;
+  vector<int> p, c, lcp;
+  vector<pci> a;
+
+  void radix(vector<pii> &v)
   {
-    vector<pii> v(n);
+    {
+      int n = v.size();
+      vector<int> cnt(n);
+      for (auto const &i : v)
+        cnt[i.fir.sec]++;
+      vector<pii> ans(n);
+      vector<int> pos(n);
+      pos[0] = 0;
+      for (int i = 1; i < n; i++)
+        pos[i] = pos[i - 1] + cnt[i - 1];
+      for (auto const &i : v)
+      {
+        int k = i.fir.sec;
+        ans[pos[k]] = i;
+        pos[k]++;
+      }
+      v = ans;
+    }
+    {
+      int n = v.size();
+      vector<int> cnt(n);
+      for (auto const &i : v)
+        cnt[i.fir.fir]++;
+      vector<pii> ans(n);
+      vector<int> pos(n);
+      pos[0] = 0;
+      for (int i = 1; i < n; i++)
+        pos[i] = pos[i - 1] + cnt[i - 1];
+      for (auto const &i : v)
+      {
+        int k = i.fir.fir;
+        ans[pos[k]] = i;
+        pos[k]++;
+      }
+      v = ans;
+    }
+  }
+  suffix_array(string &st)
+  {
+    s = st;
+    s += '$'; // menor do que todos os chars da string st
+    n = s.size();
+    p.resize(n);
+    c.resize(n);
+    a.resize(n);
     for (int i = 0; i < n; i++)
-      v[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
-    sort(v.begin(), v.end());
+      a[i] = {s[i], i};
+    sort(a.begin(), a.end());
     for (int i = 0; i < n; i++)
-      p[i] = v[i].sec;
+      p[i] = a[i].sec;
     c[p[0]] = 0;
     for (int i = 1; i < n; i++)
-      (v[i].fir == v[i - 1].fir) ? c[p[i]] = c[p[i - 1]] : c[p[i]] = c[p[i - 1]] + 1;
-    k++;
+      (a[i].fir == a[i - 1].fir) ? c[p[i]] = c[p[i - 1]] : c[p[i]] = c[p[i - 1]] + 1;
+    k = 0;
+    while ((1 << k) < n)
+    {
+      vector<pii> v(n);
+      for (int i = 0; i < n; i++)
+        v[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
+      radix(v); // pode usar std::sort()
+      for (int i = 0; i < n; i++)
+        p[i] = v[i].sec;
+      c[p[0]] = 0;
+      for (int i = 1; i < n; i++)
+        (v[i].fir == v[i - 1].fir) ? c[p[i]] = c[p[i - 1]] : c[p[i]] = c[p[i - 1]] + 1;
+      k++;
+    }
   }
-  for (int i = 0; i < n; i++)
-    cout << p[i] << " ";
-  cout << endl;
-}
+  void build_lcp()
+  {
+    lcp.resize(n);
+    k = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+      int idx = c[i], j = p[idx - 1];
+      while (s[i + k] == s[j + k])
+        k++;
+      lcp[idx] = k;
+      k = max(k - 1, 0ll);
+    }
+  }
+};
 signed main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   string s;
   cin >> s;
-  get_suf(s);
+  suffix_array sa(s);
+  for (int i = 0; i <= s.size(); i++) // sufix array
+    cout << sa.p[i] << " ";
+  cout << endl;
+  sa.build_lcp();
+  for (int i = 1; i <= s.size(); i++) // lcp entre 2 suffixos adjacentes no suffix array
+    cout << sa.lcp[i] << " ";
+  cout << endl;
   return 0;
 }
